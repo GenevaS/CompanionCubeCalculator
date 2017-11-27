d9d76848647f70236e5faca61f00b1f6159947f8
GenevaS <smithgm@mcmaster.ca>
2017-11-27 18:44:34 -0500

    Finished initial implementation of the interval data structure class

--- src/CompanionCubeCalculator/IntervalStruct.cs
+++ src/CompanionCubeCalculator/IntervalStruct.cs
@@ -17,21 +17,18 @@ namespace CompanionCubeCalculator
         /* CONSTRUCTOR */
         public IntervalStruct(double minB, double maxB)
         {
+            minBound = minB;
+            maxBound = maxB;
+
             /*
              * If the constructor has been called with minB > maxB, 
              * swap the arguements and add a warning to the program
              * log.
              */
-            if (minB <= maxB)
+            if (minBound > maxBound)
             {
-                minBound = minB;
-                maxBound = maxB;
-            }
-            else
-            {
+                SwapBounds();
                 frm_Main.UpdateLog("WARNING: Value provided for intervals are not in increasing order.The values have been exchanged to maintain the interval ordering." + System.Environment.NewLine);
-                minBound = maxB;
-                maxBound = minB;
             }
 
             return;
@@ -51,11 +48,47 @@ namespace CompanionCubeCalculator
         /* SETTERS */
         public void SetMinBound(double minB)
         {
+            minBound = minB;
+
+            /*
+             * If the new minimum bound is larger than the current
+             * maximum bound, swap the values
+             */
+            if (minBound > maxBound)
+            {
+                SwapBounds();
+                frm_Main.UpdateLog("WARNING: Value provided for minimum bound is greater than the current maximum bound. The values have been exchanged to maintain the interval ordering." + System.Environment.NewLine);
+            }
             return;
         }
 
         public void SetMaxBound(double maxB)
         {
+            maxBound = maxB;
+
+            /*
+            * If the new maximum bound is larger than the current
+            * maximum bound, swap the values
+            */
+            if (maxBound < minBound)
+            {
+                SwapBounds();
+                frm_Main.UpdateLog("WARNING: Value provided for maximum bound is smaller than the current minimum bound. The values have been exchanged to maintain the interval ordering." + System.Environment.NewLine);
+            }
+
+            return;
+        }
+
+        /*
+         * Value swapping function for maintaining the 
+         * order of the interval bounds 
+         */
+        private void SwapBounds()
+        {
+            double temp = minBound;
+            minBound = maxBound;
+            maxBound = temp;
+
             return;
         }
     }
