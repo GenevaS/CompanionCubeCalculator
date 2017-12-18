b170d0d6a44bd7ab21c8f169167a8c8afa2c30ae
GenevaS <smithgm@mcmaster.ca>
2017-12-18 16:25:58 -0500

    Working on achieving 100% code coverage for the ControlFlow module

--- src/UnitTests_CompanionCubeCalculator/ControlTests.cs
+++ src/UnitTests_CompanionCubeCalculator/ControlTests.cs
@@ -2,7 +2,7 @@
  * Control Tests
  * ---------------------------------------------------------------------
  * Author: Geneva Smith (GenevaS)
- * Updated 2017/12/15
+ * Updated 2017/12/18
  * ---------------------------------------------------------------------
  */
 
@@ -12,11 +12,30 @@ using CompanionCubeCalculator;
 namespace UnitTests_CompanionCubeCalculator
 {
     [TestClass]
-    class ControlTests
+    public class ControlTests
     {
-        public void TestResultOfFile()
+        [TestMethod]
+        public void TestInitialize()
         {
+            bool success = ControlFlow.Initialize();
+            Assert.AreEqual(true, success);
+        }
 
+        [TestMethod]
+        public void TestConditionFunction()
+        {
+            string conditioned = ControlFlow.ConditionRawInput("x + y", true);
+            Assert.AreEqual("x+y", conditioned);
+        }
+
+        [TestMethod]
+        public void TestControlFile()
+        {
+            string[] delimiters = ControlFlow.GetDelimiters();
+            string[] fileContents = ControlFlow.ControlFile(@"TestFiles/test.txt");
+            Assert.AreEqual(2, fileContents.Length);
+            Assert.AreEqual("x+y", fileContents[0]);
+            Assert.AreEqual("x" + delimiters[1] + "2" + delimiters[1] + "4" + delimiters[0] + "y" + delimiters[1] + "3" + delimiters[1] +  "5", fileContents[1]);
         }
     }
 }
